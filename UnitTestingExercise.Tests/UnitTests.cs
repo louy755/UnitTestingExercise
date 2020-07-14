770028ce400e15e52f0739e0d4570e000a77fca2
Louy Alsadi <louyalsadi@Louys-MacBook-Pro.local>
2020-07-14 12:05:04 -0500

    Add case

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,72 +6,75 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 1, 1, 3)]
+        [InlineData(0, -1, 1, 0)]
+        [InlineData(0, 0, 0, 0)]
+        [InlineData(100, 1000, 100, 1200)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            UnitTestMethods sut = new UnitTestMethods();
             //Act
-
+            var actual = sut.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(expected, actual);
         }
 
-        [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
-        {
-            //Arrange
 
-            //Act
+        //[Theory]
+        //[InlineData()]
+        //public void Subtract(int minuend, int subtrhend, int expected)
+        //{
+        //    //Arrange
 
-            //Assert
+        //    //Act
 
-        }
+        //    //Assert
 
-        [Theory]
-        [InlineData()]
-        public void Multiply(int num1, int num2, int expected)
-        {
-            //Arrange
+        //}
 
-            //Act
+        //[Theory]
+        //[InlineData()]
+        //public void Multiply(int num1, int num2, int expected)
+        //{
+        //    //Arrange
 
-            //Assert
+        //    //Act
 
-        }
+        //    //Assert
 
-        [Theory]
-        [InlineData()]
-        public void Divide(int num1, int num2, int expected)
-        {
-            //Arrange
+        //}
 
-            //Act
+        //[Theory]
+        //[InlineData()]
+        //public void Divide(int num1, int num2, int expected)
+        //{
+        //    //Arrange
 
-            //Assert
+        //    //Act
 
-        }
+        //    //Assert
 
-        [Fact]
-        public void YourMethodName()
-        {
-            //Arrange
+        //}
 
-            //Act
+        //[Fact]
+        //public void YourMethodName()
+        //{
+        //    //Arrange
 
-            //Assert
+        //    //Act
 
-        }
+        //    //Assert
 
-        [Fact]
-        public void YourMethodName2()
-        {
-            //Arrange
+        //}
 
-            //Act
+        //[Fact]
+        //public void YourMethodName2()
+        //{
+        //    //Arrange
+        //    //Act
 
-            //Assert
-        }
+        //    //Assert
+        //}
     }
 }
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -5,10 +5,15 @@ namespace UnitTestingExercise
     {
         // Make sure your references are set properly
             // Righ-click on Dependencies in your .Tests project and set your reference
+        // Done
+        public int Add(int number, int number2, int number3)
+        {
+            var result = number + number2 + number3;
+            return result;
+        }
 
+        //Done  Create an Add method that passes 3 integers
 
-
-        // Create an Add method that passes 3 integers
 
         // Create a Subtract method that passes 2 integers
             // Keep track of which number is getting passed as minuend and subtrahend
